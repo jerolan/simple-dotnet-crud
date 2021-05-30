19ead0ff4fe0021a12c72df4b340497376f657db
Jerome Olvera <jolveras@gbm.com.mx>
2021-05-30 13:37:11 -0500

    Refine structure

--- TransactionsEventSourcing/src/Transaction.cs
+++ TransactionsEventSourcing/src/Transaction.cs
@@ -2,13 +2,6 @@ namespace AwsDotnetCsharp
 {
   public class Transaction
   {
-    // public Guid Guid { get; set; }
-
     public decimal Amount { get; set; }
-
-    public Transaction()
-    {
-
-    }
   }
 }
